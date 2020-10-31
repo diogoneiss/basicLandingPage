2f640d2f16333d91ea2747bb2ddee44220df53c6
Lucas Saliba <lucassaliba10@gmail.com>
2020-10-31 15:30:25 -0300

    Components =D

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Component from "./Component";
 
 function App() {
   return (
@@ -7,17 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Código escrito por diogo neiss</h1>
+        <h1>e Lucas Saliba =D</h1>
         <p>
-          Edite <code>src/App.js</code> e salve para contribuir com meu projeto.
+          Edite <code>src/App.js</code> e salve para contribuir com 'nosso'
+          projeto.
         </p>
-        
+        <Component />
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Aprenda react aqui, vale muito a pena
+          Aprenda react aqui, vale muito a pena
         </a>
       </header>
     </div>
